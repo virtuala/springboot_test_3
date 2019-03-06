7e0dda50e8261bca34439efa12ac3637be104631
virtuala <110410524@qq.com>
2019-03-07 00:12:02 +0800

    First

--- src/main/java/com/example/springboot_test_3/SpringbootTest3Application.java
+++ src/main/java/com/example/springboot_test_3/SpringbootTest3Application.java
@@ -1,15 +1,55 @@
 package com.example.springboot_test_3;
 
+import java.util.List;
+
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@RestController
 @MapperScan("com.example.springboot_test_3.dao")
 public class SpringbootTest3Application {
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootTest3Application.class, args);
 	}
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Value("${server.port}")
+    String port;
+    @Value("${spring.application.name}")
+    String application_name;
+     
+    @RequestMapping("/hi")
+    public String sayHi(String name){
+        return "hi" + port + ";name"+name;
+    }
+    @RequestMapping("/actuator/info")
+    public List<ServiceInstance> serviceInstancesByApplicationName() {
+        return this.discoveryClient.getInstances(application_name);
+    }
+
 
 }
--- src/main/java/com/example/springboot_test_3/controller/HelloWorldController.java
+++ src/main/java/com/example/springboot_test_3/controller/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.example.springboot_test_3.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWorldController {
+	
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
     }
+
 }
